f79c287b88ae513b30a18e958404c844514c7f15
anthonymin <dbstnsdl12@naver.com>
2019-12-19 23:23:11 +0900

    Updates

---
+++ precache-manifest.994cd890c6485415fe5bf38130dec598.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ce63da2df02a9ed61509b487ddc7cc",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "6ba7fc33ca64a902699c",
+    "url": "/movie_app_2019/static/css/main.8fb28d06.chunk.css"
+  },
+  {
+    "revision": "e172effaeef260272953",
+    "url": "/movie_app_2019/static/js/2.70feb194.chunk.js"
+  },
+  {
+    "revision": "2ddb796e2cfea5f03cbea984689c4c9d",
+    "url": "/movie_app_2019/static/js/2.70feb194.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ba7fc33ca64a902699c",
+    "url": "/movie_app_2019/static/js/main.2c2c55f6.chunk.js"
+  },
+  {
+    "revision": "237b54b2d5ef1f8f1d4f",
+    "url": "/movie_app_2019/static/js/runtime-main.3f8e206a.js"
+  }
+]);
